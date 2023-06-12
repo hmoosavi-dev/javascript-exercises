7d5a0fd242da752680f45d365ee0cace96a1dd4f
hmoosavi-dev <hmoosavi.dev@gmail.com>
2023-06-12 19:38:42 -0400

    Write function reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const holderArray = [];
+  let slength = string.length;
+  for (i = 0; i < slength; i++) {
+    holderArray[i] = string.slice(i, i + 1);
+    console.log(holderArray);
+  }
+  holderArray.reverse();
+  let resultString = holderArray.join("");
+  return resultString;
 };
 
 // Do not edit below this line
