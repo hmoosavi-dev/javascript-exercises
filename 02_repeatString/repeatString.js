2ef96ef56988b83de11c6bfc03ec813d2aeeeb70
hmoosavi-dev <hmoosavi.dev@gmail.com>
2023-06-12 19:03:59 -0400

    write function repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+  let tempString = string;
+  let resultString = "";
+  if (times < 0) {
+    return "ERROR";
+  }
+  for (i = 0; i < times; i++) {
+    resultString += string;
+  }
+  return resultString;
 };
 
 // Do not edit below this line
