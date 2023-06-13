e9c1a53b75b77523982164a51a6682c059d187bc
hmoosavi-dev <hmoosavi.dev@gmail.com>
2023-06-13 00:15:42 -0400

    Create function removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...arg) {
+  const newArray = [];
 
-};
+  array.forEach((element) => {
+    if (!arg.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
