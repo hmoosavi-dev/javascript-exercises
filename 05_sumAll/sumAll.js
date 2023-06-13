2a8fd6e2d764c8928b2d7d156bf44c0f5263a82c
hmoosavi-dev <hmoosavi.dev@gmail.com>
2023-06-13 00:45:11 -0400

    write function sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sumValue = 0;
+  if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  } else if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sumValue = sumValue + i;
+    }
+    return sumValue;
+  } else {
+    for (let i = b; i <= a; i++) {
+      sumValue = sumValue + i;
+    }
+    return sumValue;
+  }
 };
 
 // Do not edit below this line
